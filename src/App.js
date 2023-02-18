63fe69d94a4e385ea0af734392557df2a25d679e
luijulus <luijulus@gmail.com>
2023-02-18 14:30:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import Personal from "./pages/Personal";
+import { useState } from "react";
+import Cardinfo from "./pages/Cardinfo";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Categories from "./pages/Categories";
 
-function App() {
+
+
+const App = () => {
+  const [isShow, setIsShow] = useState(false)
+  const [reg, setReg] = useState(false)
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header setIsShow={setIsShow} />
+      
+     
+      <Routes>
+        <Route path="/" element={<Home setIsShow={setIsShow} isShow={isShow}/>} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/cardinfo" element={<Cardinfo />} />
+        <Route path="/categories" element={<Categories/>}/>
+      </Routes>
+      <About/>
+      
+      <Footer />
+      {
+        isShow && <Login setIsShow={setIsShow} />
+      }
+    
+    </>
   );
 }
 
